c996d0337bceae71808b973b761666224e996b16
Matthew Dreier <43585853+matthewsdreier@users.noreply.github.com>
2023-02-17 12:14:46 -0500

    Add clear button functionality

--- javascript.js
+++ javascript.js
@@ -26,10 +26,32 @@ let updateDisplay = function(text) {
     display.textContent = display.textContent + text;
 }
 
-const display = document.querySelector(".display");
+let clearCalculator = function() {
+    a = null;
+    b = null;
+    operator = null;
+    display.textContent = "";
+}
 
+let a = null;
+let b = null;
+let operator = null;
+
+const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
+const equals = document.querySelector(".equals");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
 
 buttons.forEach(button => button.addEventListener("click", function() {
-    updateDisplay(this.textContent);
+    if (this.textContent != "clear") {
+        updateDisplay(this.textContent);
+    }
+
+    if (this.classList[0] === "operator") {
+        operator = this.classList[1];
+    }
+
 }));
+
+clear.addEventListener("click", clearCalculator);
