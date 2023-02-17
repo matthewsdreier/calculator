c917cdce5aefa869d9446e84a28e64d46b1ebdac
Matthew Dreier <43585853+matthewsdreier@users.noreply.github.com>
2023-02-17 13:10:36 -0500

    Fix operate function, add basic functions
    In event listeners for buttons.

--- javascript.js
+++ javascript.js
@@ -19,7 +19,20 @@ let divide = function(a, b) {
 }
 
 let operate = function(a, b, operator) {
-    return window[operator](a, b);
+    switch(operator) {
+        case "add":
+            return add(a, b);
+            break;
+        case "subtract":
+            return subtract(a, b);
+            break;
+        case "multiply":
+            return multiply(a, b);
+            break;
+        case "divide":
+            return divide(a, b);
+            break;
+    }
 }
 
 let updateDisplay = function(text) {
@@ -33,25 +46,50 @@ let clearCalculator = function() {
     display.textContent = "";
 }
 
-let a = null;
-let b = null;
+let a = "";
+let b = "";
+let ans = null;
 let operator = null;
 
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-const equals = document.querySelector(".equals");
-const operators = document.querySelectorAll(".operator");
-const clear = document.querySelector(".clear");
 
 buttons.forEach(button => button.addEventListener("click", function() {
-    if (this.textContent != "clear") {
-        updateDisplay(this.textContent);
+    if (this.textContent === "clear") {
+        clearCalculator();
+        return null;
     }
 
     if (this.classList[0] === "operator") {
-        operator = this.classList[1];
+        if (a === "" && b === "") {
+            return null;
+        } else if (b != "") {
+            let tempOperator = this.classList[1];
+            let tempAns = operate(a, b, operator);
+            clearCalculator();
+            a = tempAns;
+            operator = tempOperator;
+        } else {
+            operator = this.classList[1];
+            updateDisplay(this.textContent);
+        }
+    }
+
+    if (this.classList[0] === "number") {
+        if (operator === null) {
+            a += this.textContent;
+            updateDisplay(this.textContent);
+        } else {
+            b += this.textContent;
+            updateDisplay(this.textContent);
+        }
+
+        // need to add clear calc if equals in on screen here
+    }
+
+    if (this.classList[0] === "equals") {
+        ans = operate(a, b, operator)
+        updateDisplay(this.textContent + ans);
     }
 
 }));
-
-clear.addEventListener("click", clearCalculator);
